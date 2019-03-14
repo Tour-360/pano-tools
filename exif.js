bac4e2b16257247227123ae1fae8f1370fd31ed8
Denis Borodin <borodindk@gmail.com>
2019-03-14 15:39:17 +0300

    fix pt exif

--- exif.js
+++ exif.js
@@ -7,7 +7,7 @@ const exiftool = path.resolve(__dirname, execs.exiftool);
 const jpegDir = path.resolve(stages[6]);
 module.exports = () => {
   return new Promise((resolve, reject) => {
-    exec(`${exiftool} -xmp:ProjectionType=equirectangular -xmp:UsePanoramaViewer="True" -xmp:PoseHeadingDegrees=360 -xmp:CroppedAreaLeftPixels=0 -xmp:CroppedAreaTopPixels=0 -xmp:CroppedAreaImageWidthPixels=8000 -xmp:CroppedAreaImageHeightPixels=4000 -xmp:FullPanoWidthPixels=8000 -xmp:FullPanoHeightPixels=4000 -overwrite_original ${jpegDir}/*.jpg`, (e) => {
+    exec(`${exiftool} -xmp:ProjectionType=equirectangular -xmp:UsePanoramaViewer="True" -xmp:PoseHeadingDegrees=360 -xmp:CroppedAreaLeftPixels=0 -xmp:CroppedAreaTopPixels=0 -xmp:CroppedAreaImageWidthPixels=8000 -xmp:CroppedAreaImageHeightPixels=4000 -xmp:FullPanoWidthPixels=8000 -xmp:FullPanoHeightPixels=4000 -overwrite_original '${jpegDir}/'*.jpg`, (e) => {
       !e ? resolve('Exif успешно вшит') : reject(e);
     });
   });
