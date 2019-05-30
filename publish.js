9b3f182511d74add9beb637b74d2aedc43813cff
Denis Borodin <borodindk@gmail.com>
2019-05-30 12:59:16 +0300

    publish with rsync

--- publish.js
+++ publish.js
@@ -1,6 +1,6 @@
 module.exports = () => {
   const path = require("path");
-  const { execSync, exec } = require('child_process');
+  const { execSync, exec, spawn } = require('child_process');
   const { getProject } = require('./utils.js');
   const { stages } = require('./config.json');
   const webDir = path.resolve(stages[9]);
@@ -10,9 +10,23 @@ module.exports = () => {
     const projectFolder = project.folder || project.name;
     const path = `/var/www/tour-360.ru/projects/${projectFolder}`;
     execSync(`ssh server@tour-360.ru 'mkdir -p "${path}"'`);
-    exec(`scp -r -P 22 '${webDir}/'* server@tour-360.ru:/var/www/tour-360.ru/projects/${projectFolder}`, err => {
-      if(!err) resolve('Проект успешно опубликован');
-      else reject(err);
+
+    const rsync = exec(`rsync --progress -ru -L --iconv=utf-8 '${webDir}/'* server@tour-360.ru:/var/www/tour-360.ru/projects/${projectFolder}`);
+
+    rsync.stdout.on('data', function (data) {
+      console.log(data.toString());
+    });
+
+    // rsync.stderr.on('data', function (data) {
+    //   console.log('Error: ' + data.toString());
+    // });
+
+    rsync.on('exit', function (code) {
+      if (code == 0) {
+        resolve('Проект успешно опубликован')
+      } else {
+        reject(err);
+      }
     });
   });
 }
