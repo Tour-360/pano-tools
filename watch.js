a15f5422a0a9b528fb514012fb118d615761a79a
Denis Borodin <borodindk@gmail.com>
2019-06-06 13:44:06 +0300

    fix watch

--- watch.js
+++ watch.js
@@ -1,18 +1,21 @@
 const chokidar = require("chokidar");
 const { files, bar } = require('./utils.js');
 
-module.exports = (fromDir, toDir) => {
+module.exports = (fromDir, toDir, msg) => {
   return new Promise((resolve, reject) => {
+
     const fromFiles = files(fromDir);
     const toFiles = files(toDir);
     let progress = toFiles.length;
 
+    console.log(`Началось отслеживание каталога ${toDir}`.bold);
     bar.start(fromFiles.length+1, progress);
     const watcher = chokidar.watch(toDir).on('change', (filePath) => {
-      bar.update(progress);
+      bar.update(++progress);
       if (progress >= fromFiles.length) {
         bar.stop();
         watcher.close();
+        resolve('Все файлы успешно записанны');
       }
     });
   })
