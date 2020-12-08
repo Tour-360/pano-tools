ac7f2f18e82aa626e04d5b9b439998771efa6a87
Denis Borodin <borodindk@gmail.com>
2020-12-08 22:08:56 +0300

    fix cub e

--- cube.js
+++ cube.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const fs = require("fs-extra");
 const { exec, execSync } = require('child_process');
-const { files, bar } = require('./utils.js');
+const { files, bar, ifExistSync } = require('./utils.js');
 const { stages, execs } = require('./config.json');
 const ptguiQueue = [];
 const panoFiles = [];
@@ -14,48 +14,59 @@ const completeMessage = "Конвертация панорам в стороны
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
+    !ifExistSync(cubeDir) && fs.mkdirSync(cubeDir);
     files(jpegDir, 'jpg').map(panoName => {
       panoName = panoName.split('.')[0];
-      panoFolder = path.resolve(cubeDir, panoName.toString());
-      !fs.existsSync(cubeDir) && fs.mkdirSync(cubeDir);
-      if (!fs.existsSync(panoFolder)){
+      const panoFolder = path.resolve(cubeDir, panoName.toString());
+      if (!ifExistSync(panoFolder)) {
         fs.mkdirSync(panoFolder);
-        const panoFile = path.resolve(panoFolder, 'pano.jpg');
-        panoFiles.push(panoFile);
-        const jpegPath = path.resolve(jpegDir, panoName + ".jpg");
-        fs.copySync(jpegPath, panoFile);
-        let image = execSync(`${exiftool} '${jpegPath}' -s -s  -ImageWidth -ImageHeight`)
-          .toString('utf8')
-          .split('\n')
-          .map(s => s.split(': ')[1]);
-
-        image = {
-          width: image[0],
-          height: image[1]
-        }
-
-        const size = Math.pow(2, Math.round(Math.log(image.width / Math.PI) / Math.log(2)));
-
-        ['0', '1','2','3','4','5','ultra_wide_nadir'].forEach((i) => {
-          let templatePath = path.resolve(__dirname,  "templates/ptgui/cube/",  i + ".pts");
-          var ptsPath = path.resolve(panoFolder, i + ".pts");
-          const template =  fs.readFileSync(templatePath);
-          fs.writeFileSync(
-            ptsPath,
-            template.toString('utf8')
-              .replace(/IMAGE_WIDTH/g, image.width)
-              .replace(/IMAGE_HEIGHT/g, image.height)
-              .replace(/EXPORT_SIZE/g, i === 'ultra_wide_nadir' ? 256 : size)
-          );
-          ptguiQueue.push(ptsPath);
-        });
       }
+      const panoFile = path.resolve(panoFolder, 'pano.jpg');
+      const jpegPath = path.resolve(jpegDir, panoName + ".jpg");
+      let image = execSync(`${exiftool} '${jpegPath}' -s -s  -ImageWidth -ImageHeight`)
+        .toString('utf8')
+        .split('\n')
+        .map(s => s.split(': ')[1]);
+
+      image = {
+        width: image[0],
+        height: image[1]
+      }
+
+      const size = Math.pow(2, Math.round(Math.log(image.width / Math.PI) / Math.log(2)));
+
+      ['0', '1','2','3','4','5','ultra_wide_nadir'].forEach((i) => {
+        let templatePath = path.resolve(__dirname,  "templates/ptgui/cube/",  i + ".pts");
+        const ptsPath = path.resolve(panoFolder, i + ".pts");
+        const jpgPath = path.resolve(panoFolder, i + ".jpg");
+        if (!ifExistSync(jpgPath)) {
+
+          if (!fs.existsSync(panoFile)) {
+            fs.symlinkSync(jpegPath, panoFile);
+            panoFiles.push(panoFile);
+          }
+
+
+          if (!ifExistSync(ptsPath)) {
+            const template = fs.readFileSync(templatePath);
+            fs.writeFileSync(
+              ptsPath,
+              template.toString('utf8')
+                .replace(/IMAGE_WIDTH/g, image.width)
+                .replace(/IMAGE_HEIGHT/g, image.height)
+                .replace(/EXPORT_SIZE/g, i === 'ultra_wide_nadir' ? 256 : size)
+            );
+          }
+          ptguiQueue.push(ptsPath);
+        }
+      });
     })
 
     if (ptguiQueue.length){
-      console.log("Конвертация панорам в стороны куба".bold);
-      exec(`open '/Applications/PTGui Pro.app' -n -W --args -batch -d -x ${ptguiQueue.map(p => `'${p}'`).join(' ')}`, () => {
-        panoFiles.map(panoFile => {
+      console.log(`Конвертация ${ptguiQueue.length} панорам в стороны куба`.bold);
+      console.log(`Внимание! количество обрабатываемых сторон очень велико, программа сделает только первые 1000, для того что бы сделать остальные повторите команду`.red)
+      exec(`open '/Applications/PTGui Pro.app' -n -W --args -batch -d -x ${ptguiQueue.splice(0,1000).map(p => `'${p}'`).join(' ')}`, () => {
+        panoFiles.splice(0,1000).map(panoFile => {
           fs.unlink(panoFile);
         })
 
