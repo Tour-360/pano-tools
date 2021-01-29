4cecbb2fb4b731b3c625fbed9f347182fef3f5f1
Denis Borodin <borodindk@gmail.com>
2021-01-29 18:15:00 +0300

    fix player-update

--- comands/player-update.js
+++ comands/player-update.js
@@ -1,4 +1,7 @@
-var semverSort = require('semver-sort');
+const semverSort = require('semver-sort');
+const semverDiff = require('semver-diff');
+const semverValid = require('semver/functions/valid')
+
 const { files, bar } = require('../utils.js');
 const fse = require('fs-extra');
 const { JSDOM } = require("jsdom");
@@ -6,55 +9,82 @@ const path = require("path");
 const { notification } = require('../utils');
 const { stages, presets, execs } = require('../config.json');
 const ftp = require("basic-ftp");
-
+const webDir = path.resolve(stages[9]);
 
 exports.command = 'player-update';
 exports.desc = 'Обновление верисии плеера на актуальную';
 exports.builder = {
-  path: {
-    alias: 'p',
-    desc: 'File path'
+  file: {
+    alias: 'f',
+    default: `${webDir}/index.html`,
+    desc: 'HTML file path'
+  },
+  diff: {
+    alias: 'd',
+    default: 'minor',
+    desc: 'Difference semver'
   }
 };
 
 
-exports.handler = async ({ path: indexPath }) => {
-    const webDir = path.resolve(stages[9]);
+exports.handler = async ({ file: indexPath, diff }) => {
     if (!indexPath) {
-      indexPath = path.resolve(webDir, 'index.html');
+      indexPath = path.resolve(webDir);
     }
 
-    try {
-      const client = new ftp.Client();
-      await client.access({ host: 'tour-360.ru' });
-      const versionList = semverSort.asc(
-        (await client.list('/tour-player'))
-        .map(f => f.name)
-        .filter(f => /([0-9]*)\.([0-9]*)\.([0-9]*)/.test(f))
-      );
+    const html = fse.readFileSync(indexPath).toString('utf8');
+    const dom = new JSDOM(html);
+    const document = dom?.window?.document;
 
-      const latest = versionList[versionList.length - 1];
+    if (document) {
 
-      notification.info(`Download Tour-player v${latest}`);
-      const tourPlayerDir = path.resolve(webDir, 'libs', 'tour-player', latest);
-      fse.ensureDirSync(tourPlayerDir);
-      await client.downloadToDir(tourPlayerDir, `/tour-player/${latest}`);
-      await client.close();
-
-      notification.info(`Обновление ссылок на файл плеера в файле ${indexPath}`);
-
-      const html = fse.readFileSync(indexPath).toString('utf8');
-
-      const dom = new JSDOM(html);
-      const document = dom?.window?.document;
-      if (document) {
-
-        const css = document.querySelector('link[rel="stylesheet"][href$="tour-player.css"]');
-        const script = document.querySelector('script[src$="tour-player.js"]');
+      const css = document.querySelector('link[rel="stylesheet"][href$="tour-player.css"]');
+      const script = document.querySelector('script[src$="tour-player.js"]');
 
 
-        const oldPath = script.src.split('/');
-        const oldVersion = oldPath[oldPath.length - 2];
+      const oldPath = script.src.split('/');
+      const oldVersion = '2.2.0' || oldPath[oldPath.length - 2];
+
+      const semver = {
+        major: ['release', 'prerelease', 'major', 'premajor', 'minor', 'preminor', 'patch', 'prepatch', 'build'],
+        minor: ['minor', 'preminor', 'patch', 'prepatch', 'build'],
+        patch: ['patch', 'prepatch', 'build'],
+      }
+
+      try {
+        const client = new ftp.Client();
+        await client.access({ host: 'tour-360.ru' });
+        const versionList = semverSort.asc(
+          (await client.list('/tour-player'))
+            .map(f => f.name)
+            .filter(newVersion =>
+              semverValid(oldVersion) &&
+              semverValid(newVersion) &&
+              semver[diff].includes(semverDiff(oldVersion, newVersion))
+              || oldVersion === newVersion
+            )
+        );
+
+        if (!versionList.length) {
+          await notification.warning('Нет доступных версий для обьновленияю Возможно вы хотите сделать мажорное обновление (pt player-update -f major)')
+          process.exit(0);
+        }
+
+        const latest = versionList[versionList.length - 1];
+
+        notification.info(`Download Tour-player v${latest}`);
+        const tourPlayerDir = path.resolve(webDir, 'libs', 'tour-player', latest);
+        fse.ensureDirSync(tourPlayerDir);
+        await client.downloadToDir(tourPlayerDir, `/tour-player/${latest}`);
+        await client.close();
+
+        notification.info(`Обновление ссылок на файл плеера в файле ${indexPath}`);
+
+        const html = fse.readFileSync(indexPath).toString('utf8');
+
+        const dom = new JSDOM(html);
+        const document = dom?.window?.document;
+
 
         css.href = `libs/tour-player/${latest}/tour-player.css`;
         script.src = `libs/tour-player/${latest}/tour-player.js`;
@@ -66,13 +96,14 @@ exports.handler = async ({ path: indexPath }) => {
 
         notification.success(`Update tour-player ${oldVersion} -> ${latest}`);
 
-      } else {
-        notification.error('Ошибка парсинга html');
+      } catch (e) {
+        notification.error('Ошибка получения файлов плеера с сервера');
+        throw e;
       }
 
-    } catch (e) {
-      notification.error('Ошибка получения файлов плеера с сервера');
-      throw e;
+
+    } else {
+      notification.error('Ошибка парсинга html');
     }
 }
 
